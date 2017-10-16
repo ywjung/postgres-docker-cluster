5055c87e03a028943d764fb4e5ec80e221e961c6
syndbg <syndbe@gmail.com>
2017-10-17 00:21:21 +0300

    Add utility scripts for Docker hub

---
+++ build.sh
@@ -0,0 +1,7 @@
+docker build -f Postgres-9.6.Dockerfile -t syndbg/postgres-cluster:9.6 .
+docker build -f Postgres-9.5.Dockerfile -t syndbg/postgres-cluster:9.5 .
+docker build -f Postgres-9.6.Dockerfile -t syndbg/postgres-cluster:9.6 .
+docker build -f Postgres-9.5.Dockerfile -t syndbg/postgres-cluster:9.5 .
+docker build -f Barman-2.2.Dockerfile -t syndbg/barman:2.2 .
+docker build -f Pgpool-3.3.Dockerfile -t syndbg/pgpool:3.3 .
+docker build -f Pgpool-3.6.Dockerfile -t syndbg/pgpool:3.6 .
---
+++ deploy.sh
@@ -0,0 +1,7 @@
+docker push syndbg/postgres-cluster:9.6
+docker push syndbg/postgres-cluster:9.5
+docker push syndbg/postgres-cluster:9.6
+docker push syndbg/postgres-cluster:9.5
+docker push syndbg/barman:2.2
+docker push syndbg/pgpool:3.3
+docker push syndbg/pgpool:3.6
